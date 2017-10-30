70852892abd238d73e682a5e942c57bff3b525a4
Beck Chen <csybeck@gmail.com>
2017-10-30 14:50:19 -0700

    Command-line flag for yaml file

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"net/http"
-
 	"github.com/meditativeape/urlshort/impl"
+	"io/ioutil"
+	"net/http"
 )
 
 func main() {
+	yamlPath := flag.String("yaml", "", "Path to a YAML config file")
+	// jsonPath := flag.String("json", "", "Path to a JSON config file")
+	flag.Parse()
 	mux := defaultMux()
 
 	// Build the MapHandler using the mux as the fallback
@@ -15,22 +19,20 @@ func main() {
 		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
 		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
 	}
-	mapHandler := impl.MapHandler(pathsToUrls, mux)
+	handler := impl.MapHandler(pathsToUrls, mux)
 
-	// Build the YAMLHandler using the mapHandler as the
-	// fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
-	yamlHandler, err := impl.YAMLHandler([]byte(yaml), mapHandler)
-	if err != nil {
-		panic(err)
+	// If yaml flag is set, read the yaml file, and build the YAMLHandler using
+	// mapHandler as the fallback
+	fmt.Println(*yamlPath)
+	if len(*yamlPath) > 0 {
+		yaml, err := ioutil.ReadFile(*yamlPath)
+		check(err)
+		handler, err = impl.YAMLHandler([]byte(yaml), handler)
+		check(err)
 	}
+
 	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
+	http.ListenAndServe(":8080", handler)
 }
 
 func defaultMux() *http.ServeMux {
@@ -41,4 +43,10 @@ func defaultMux() *http.ServeMux {
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "No redirect for this URL!")
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
